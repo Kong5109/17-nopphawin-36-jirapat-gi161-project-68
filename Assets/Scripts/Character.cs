9d30bf4ac3d4193464580d00785feed5e67c98d7
meeteedee <meepoohfisho@gmail.com>
2025-11-29 23:40:03 +0700

    SetUp Code in Character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
-public class Character : MonoBehaviour
+
+public abstract class Character : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [Header("Base Character Stats")]
+    
         
+    [SerializeField] protected string _characterName;
+    [SerializeField] protected int _hp;
+    [SerializeField] protected float _moveSpeed; 
+    [SerializeField] protected int _scoreValue;
+
+   
+    public string CharacterName { get { return _characterName; } }
+    public int HP { get { return _hp; } }
+    public int ScoreValue { get { return _scoreValue; } }
+    
+    public virtual void TakeDamage(int damage)
+    {
+        _hp -= damage;
+        
+        Debug.Log(_characterName + " took " + damage + " damage. Current HP: " + _hp);
+
+        if (_hp <= 0)
+        {
+            Die();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    public abstract void Die();
 }
