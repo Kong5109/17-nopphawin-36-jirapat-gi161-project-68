24b502d0291faa79baf0741076b816f66bbb2935
meeteedee <meepoohfisho@gmail.com>
2025-11-30 01:28:17 +0700

    PlayerBulletShoot

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,15 +2,59 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Bullet Stats")]
+    [SerializeField] private float _speed = 20f;
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private float _lifeTime = 3f; 
+
+    
+    private string _ownerTag; 
+
+    private void Start()
     {
-        
+        Destroy(gameObject, _lifeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Move();
+    }
+
+
+    public void Move()
+    {
+       
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+
+    public void Init(string ownerTag, int damageValue)
+    {
+        _ownerTag = ownerTag;
+        _damage = damageValue;
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+      
+        if (other.CompareTag(_ownerTag)) return;
+
+       
+        Character target = other.GetComponent<Character>();
+
         
+        if (target != null)
+        {
+            OnHit(target);
+        }
+
+       
+        Destroy(gameObject); 
+    }
+
+    
+    public void OnHit(Character target)
+    {
+        target.TakeDamage(_damage);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 public class Player : Character
 {
-    [Header("Player Specific Stats")]
-    [SerializeField] private int _lives = 3;       
-    [SerializeField] private float _fireRate = 0.5f; 
+    [Header("Player Specific Stats")] [SerializeField]
+    private int _lives = 3;
 
-    [Header("Shooting Setup")]
-    [SerializeField] private GameObject _bulletPrefab; 
-    [SerializeField] private Transform _firePoint;     
+    [SerializeField] private float _fireRate = 0.5f;
 
-    private float _nextFireTime = 0f; 
+    [Header("Shooting Setup")] [SerializeField]
+    private GameObject _bulletPrefab;
+
+    [SerializeField] private Transform _firePoint;
+
+    private float _nextFireTime = 0f;
 
     private void Start()
     {
-        
+
         if (_characterName == "") _characterName = "Player One";
     }
 
@@ -33,47 +35,50 @@ public class Player : Character
 
         if (_lives > 0)
         {
-            _hp = 100; 
+            _hp = 100;
         }
         else
         {
             Debug.Log("GAME OVER");
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
     }
 
 
-    
+
     public void Move()
     {
-        
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        
+
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        
+
         transform.Translate(direction * _moveSpeed * Time.deltaTime);
-        
+
     }
 
-    
+
     public void Shoot()
     {
-        
+
         if (Input.GetKey(KeyCode.Space) && Time.time > _nextFireTime)
         {
-            
             _nextFireTime = Time.time + _fireRate;
 
-            
             if (_bulletPrefab != null && _firePoint != null)
             {
-                
                 GameObject bulletObj = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.identity);
-                
-                
+
+                // --- ส่วนที่เพิ่มเข้ามา ---
+                Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+                if (bulletScript != null)
+                {
+                    // ส่งข้อมูลไปบอกกระสุนว่า "Player เป็นคนยิงนะ" และ "ดาเมจเท่าไหร่นะ"
+                    bulletScript.Init("Player", 10);
+                }
             }
         }
     }
