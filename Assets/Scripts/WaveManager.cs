7fdd744718c230e5656d11899c341c742c1eb88f
meeteedee <meepoohfisho@gmail.com>
2025-11-30 05:40:10 +0700

    WaveManager done

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class WaveSetting
+    {
+        public string waveName = "Mixed Wave";
+        
+        public Character[] possibleSpawns; 
+        
+        public int count = 10;
+        public float rate = 1.0f;
+    }
+
+    [Header("Wave Configuration")]
+    [SerializeField] private List<WaveSetting> _waves;
+    [SerializeField] private float _timeBetweenWaves = 3.0f;
+
+    [Header("Spawn Settings")]
+    [SerializeField] private Transform[] _spawnPoints;
+
+    public int CurrentWaveIndex { get; private set; }
+    public bool IsBossSpawned { get; private set; }
+
+    private bool _isSpawning = false;
+
+    private void Start()
+    {
+        CurrentWaveIndex = 0;
+        StartWaves();
+    }
+
+    private void Update()
+    {
+        UpdateWaves();
+    }
+
+    public void StartWaves()
+    {
+        if (_waves.Count > 0)
+        {
+            StartCoroutine(SpawnWaveRoutine(CurrentWaveIndex));
+        }
+    }
+
+    public void UpdateWaves()
+    {
+        if (_isSpawning) return;
+
+        if (IsAllWaveCleared())
+        {
+            CurrentWaveIndex++;
+            if (CurrentWaveIndex < _waves.Count)
+            {
+                StartCoroutine(SpawnWaveRoutine(CurrentWaveIndex));
+            }
+            else
+            {
+                Debug.Log("ALL WAVES CLEARED! YOU WIN!");
+                enabled = false;
+            }
+        }
+    }
+
+    public bool IsAllWaveCleared()
+    {
+        
+        return GameObject.FindGameObjectsWithTag("Enemy").Length == 0;
+    }
+
+    private IEnumerator SpawnWaveRoutine(int index)
+    {
+        _isSpawning = true;
+        yield return new WaitForSeconds(_timeBetweenWaves);
+
+        WaveSetting currentWave = _waves[index];
+        Debug.Log("Starting " + currentWave.waveName);
+
+        foreach(var spawn in currentWave.possibleSpawns)
+        {
+            if (spawn is BossCommandShip)
+            {
+                IsBossSpawned = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < currentWave.count; i++)
+        {
+            if (currentWave.possibleSpawns.Length > 0)
+            {
+                int randomIndex = Random.Range(0, currentWave.possibleSpawns.Length);
+                Character randomPrefab = currentWave.possibleSpawns[randomIndex];
+                
+                SpawnObject(randomPrefab);
+            }
+            
+            yield return new WaitForSeconds(currentWave.rate);
+        }
+
+        _isSpawning = false;
+    }
+
+    private void SpawnObject(Character prefab)
+    {
+        Transform randomPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+        Instantiate(prefab, randomPoint.position, Quaternion.identity);
+    }
+}
