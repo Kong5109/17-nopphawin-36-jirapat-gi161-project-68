345b95e9241aaaa23c979b0279731bc9b3c1b7d4
meeteedee <meepoohfisho@gmail.com>
2025-11-30 01:39:38 +0700

    SetUp abstract class Enemy enum EnemyType

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public enum EnemyType
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    BasicWarShip,
+    FastScout,
+    HeavyDrone,
+    BossCommandShip
+}
+
+public abstract class Enemy : Character
+{
+    [Header("Enemy Stats")]
+    [SerializeField] protected EnemyType _enemyType;
+    [SerializeField] protected float _shootInterval = 2f; 
+
+    [Header("Shooting Setup")]
+    [SerializeField] protected GameObject _bulletPrefab;
+    [SerializeField] protected Transform _firePoint;
+
+    protected float _nextShootTime;
+
+    protected virtual void Start()
     {
-        
+        _nextShootTime = Time.time + Random.Range(0f, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
+        AttackPattern();
+    }
+
+    
+    public abstract void AttackPattern();
+
+
+   
+    public void Shoot()
+    {
+        if (Time.time > _nextShootTime)
+        {
+            if (_bulletPrefab != null && _firePoint != null)
+            {
+                GameObject bulletObj = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.Euler(0, 0, 180)); 
+
+                Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+                if (bulletScript != null)
+                {
+                    bulletScript.Init("Enemy", 10); 
+                }
+            }
+            _nextShootTime = Time.time + _shootInterval;
+        }
+    }
+    
+    public override void Die()
+    {
+      
+        Debug.Log("Score + " + _scoreValue);
         
+
+        //  เอฟเฟกต์ระเบิด
+        // Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+       
+        Destroy(gameObject);
     }
 }
